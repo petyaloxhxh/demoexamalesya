0d562d72930c0f30c862e61ddf6348fc30906c8f
alaaaaaa <alesya.alley@yandex.ru>
2023-06-01 21:42:56 +0300

    second

--- app/src/main/java/com/example/demoexamalesya/MainActivity.java
+++ app/src/main/java/com/example/demoexamalesya/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.demoexamalesya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void one_click(View view) {
+        Intent intent = new Intent(this,Finish.class);
+        startActivity(intent);
+    }
+
 }
